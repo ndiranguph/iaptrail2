3383eb56f8e034c85e927c927d29ca42fd924405
ndiranguph <philip.gakuo@strathmore.edu>
2024-08-28 15:59:20 +0300

    1 commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport">
+        <title>Sem</title>
+    </head>
+    <body>
+        <h1>This is going to be a wonderful semester</h1>
+        <?php
+        require_once "load.php";
+        ?>
+    </body>
+    
+</html>
---
+++ load.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once "fnc.php";
+$Obj = new fnc();
+
+?>
